b9bfbe557875a995331fd39629c9e7674ccdd5d3
sumayyayellow <sumayyayellow@gmail.com>
2022-10-11 07:56:04 -0500

    lab7

---
+++ lab7/code.js
@@ -0,0 +1,106 @@
+/* max returns the maximum of 2 arguments */
+function max(a, b) {
+    let c = [a, b];
+
+    const d = a.filter(function (prev, elem, i, array) {
+      return prev>elem? prev: elem;
+    }, 0);
+
+    return c;
+  }
+
+  /* max3 takes 3 numbers as arguments and returns the largest */
+  function maxOfThree(a, b, c) {
+    
+    let arr = [a, b, c];
+    let result = arr.reduce((accumulator, currentValue) => {
+        return (accumulator > currentValue? accumulator: currentValue);
+    },0);
+
+    return result;
+  }
+
+  function isVowel(x) {
+    const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+    
+    const isV = vowels.filter(function(ele, i, array){
+        return (ele === x ) 
+    });
+
+    return isV.length > 0;
+  }
+  
+  function sum(a) {
+    let sum = 0;
+    let a = [1,2,3,4];
+    const result = a.reduce((accumulator, currentValue) => {
+      return accumulator + currentValue;
+    },0);
+  }
+
+
+  function multiply(arr){
+    return arr.reduce(function (prev, elm, i, array) {
+        return prev*elm;
+    }, 1);
+}
+  
+
+  function reverse(str) {
+    let revStrArr = str.split("");
+
+    return revStrArr.reverse().reduce(function(prev, elm, i, array) {
+        return prev +""+elm;
+    },"");
+}
+
+function findLongestWord(wordsArry) {
+    let len = wordsArry.reduce(function(prev, elem, i, array) {
+        return prev.length > elem.length?prev: elem;
+    },'');
+
+    return len.length;
+}
+  
+
+function filterLongWords(wordsArry, maxLen) {
+    return wordsArry.filter(function(elem, i, array) {
+        return elem.length > maxLen;
+    });
+}
+
+  
+
+  function mapMultiply() {
+    const a = [1, 3, 5, 3, 3];
+    const b = a.map(function (elem, i, array) {
+      return elem * 10;
+    });
+
+    return b;
+  }
+
+  
+
+  function findFilter() {
+    const a = [1, 3, 5, 3, 3];
+
+    const c = a.filter(function (elem, i, array) {
+      return elem == 3;
+    });
+
+    return c;
+  }
+
+  
+
+  function reduceEle() {
+    const a = [1, 3, 5, 3, 3];
+    const d = a.reduce(function (prevValue, elem, i, array) {
+      return prevValue * elem;
+    });
+
+    return d;
+  }
+
+  
---
+++ lab7/test.js
@@ -0,0 +1,53 @@
+describe("max", function () {
+  it("takes 2 number, and returns whicherver number is maximum", function () {
+    assert.equal(7, max(5, 7));
+  });
+});
+
+describe("maxThree", function () {
+  it("takes 3 number, and returns whicherver number is maximum", function () {
+    assert.equal(7, maxThree(5, 7, 2));
+  });
+});
+
+describe("isVowel", function () {
+  it("takes 1 character, and returns Boolean true/false for vowel", function () {
+    assert.equal(true, isVowel("a"));
+  });
+});
+
+describe("sum", function () {
+  it("takes [2,4,5] array of number, and returns total sum", function () {
+    assert.equal(11, sum([2, 4, 5]));
+  });
+});
+describe("multiply", function () {
+  it("takes [2,4,5] array of number, and returns total multiply", function () {
+    assert.equal(40, multiply([2, 4, 5]));
+  });
+});
+describe("reverse", function () {
+  it("takes string, and returns reverse string", function () {
+    assert.equal("olleh", reverse("hello"));
+  });
+});
+describe("findLongestWord", function () {
+  it("takes ['hello','ss','ddd'] array of string, and returns longest word length", function () {
+    assert.equal(5, findLongestWord(["hello", "ss", "ddd"]));
+  });
+});
+
+describe("filterLongWords", function () {
+  it("takes ['hello','ss','ddd'] , length  array filter to a length, and returns longest word of given length", function () {
+    assert.equal(
+      ["hello", "ddd"].toString(),
+      filterLongWords(["hello", "ss", "ddd"], 2)
+    );
+  });
+});
+
+describe("shortest", function () {
+  it("takes 2,4,1 number, and returns shorest/minimum number", function () {
+    assert.equal(1, shortest(2, 4, 1));
+  });
+});
